6e7ae693717fdd1d6595a7c33bbce8bd15796219
Emil Boman <emil.boman@gmail.com>
2016-10-26 20:35:51 +0200

    first commit

---
+++ game.js
@@ -0,0 +1,58 @@
+$(function() {
+    initCanvas();
+    initSocket("192.168.1.150", 9002);
+
+    socket.onopen = function(event) {
+    };
+
+    socket.onmessage = function(event) {
+        var msg = JSON.parse(event.data);
+        if (msg.action == "click") {
+            clearCanvas();
+            drawCircle(msg.x, msg.y);
+        }
+    }
+
+    $("#canvas").click(function(event) {
+        var msg = {
+            action: "click",
+            x: event.pageX,
+            y: event.pageY
+        };
+        socket.sendJSON(msg);
+    });
+
+    drawCircle(100, 100);
+});
+
+function initCanvas() {
+    window.canvas = document.getElementById('canvas');
+    window.context = document.getElementById('canvas').getContext('2d');
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function initSocket(server, port) {
+    window.socket = new WebSocket("ws://" + server + ":" + port);
+    socket.sendJSON = sendJSON;
+}
+
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawCircle(x, y) {
+    var radius = 10;
+
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = '#003300';
+    context.stroke();
+}
+
+function sendJSON(msg) {
+    this.send(JSON.stringify(msg));
+}
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>snek</title>
+    <style>
+        * { margin: 0; padding: 0;}
+
+        body, html { height:100%; }
+
+        #canvas {
+            position:absolute;
+        }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+</head>
+<body>
+	<canvas id="canvas"></canvas>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="game.js"></script>
+</body>
+</html>
